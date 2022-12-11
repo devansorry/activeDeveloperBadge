108bb0e2b82af028602887834a8c5ebf1226360a
Ansorry <83605318+devansorry@users.noreply.github.com>
2022-12-10 20:40:12 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
+
+const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
+const { User, Message, GuildMember, ThreadMember } = Partials;
+
+const { loadEvents } = require('./handlers/eventHandler');
+const { loadCommands } = require('./handlers/commandHandler');
+
+const client = new Client({
+    intents: [ Guilds, GuildMembers, GuildMessages ],
+    partials: [ User, Message, GuildMember, ThreadMember ]
+});
+
+client.commands = new Collection();
+client.config = require('./config.json');
+
+client.login(client.config.token).then(() => {
+    loadEvents(client);
+    loadCommands(client);
+});
